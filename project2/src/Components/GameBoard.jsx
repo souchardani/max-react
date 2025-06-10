4b57031cd14089bddb4f8f342dc5760311d6606c
Daniel <danielsou9782@gmail.com>
2025-06-10 13:21:24 +0200

    finished tic tac toe

--- project2/src/Components/GameBoard.jsx
+++ project2/src/Components/GameBoard.jsx
@@ -1,32 +1,21 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innedArray) => [...innedArray]),
-      ]; //copy of the old array
-      updatedBoard[rowIndex][colIndex] = "X";
-      return updatedBoard;
-    });
-  }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
--- project2/src/Components/Player.jsx
+++ project2/src/Components/Player.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -10,14 +15,17 @@ export default function Player({ initialName, symbol }) {
     setIsEditing((value) => {
       return !value;
     });
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function HandleChange(event) {
-    console.log(event);
     setPlayerName(event.target.value);
   }
 
-  let EditablePlayerName = <span className="player-name">{initialName}</span>;
+  let EditablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing)
     EditablePlayerName = (
@@ -25,7 +33,7 @@ export default function Player({ initialName, symbol }) {
     );
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {EditablePlayerName}
         <span className="player-symbol">{symbol}</span>
