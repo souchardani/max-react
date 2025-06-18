ce809538d990ef8f1307eaeb6e375147ed5769ac
Daniel <danielsou9782@gmail.com>
2025-06-18 11:57:05 +0200

    new app max solutoin

--- project3_2/src/App.jsx
+++ project3_2/src/App.jsx
@@ -1,11 +1,33 @@
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p>please enter a valid number</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
